10d33c05912d6d1769b45751798833368bd4b238
Viviancat666 <vivian9069@gmail.com>
2023-10-19 02:22:04 +0300

    Matrix

---
+++ Fundamentals/matrix/08.py
@@ -0,0 +1,18 @@
+x = 5
+y = 4
+
+number = 1
+matrix = []
+
+for column in range(y):
+    rows = []
+    for row in range(x):
+        rows.append(number)
+        number += 1
+    matrix.append(rows)
+    
+print(matrix)
+
+for row in matrix:
+    print(row)
+
