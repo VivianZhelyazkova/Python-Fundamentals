09ed3e6a5ec3dabc4f334213e49998e99e540281
Viviancat666 <vivian9069@gmail.com>
2023-11-02 20:27:56 +0200

    Create softuni_parking.py

---
+++ Fundamentals/dictionaries_exercise/softuni_parking.py
@@ -0,0 +1,24 @@
+number_of_commands = int(input())
+system = {}
+for command in range(number_of_commands):
+    action = input().split()
+    if "unregister" == action[0]:
+        action_type = action[0]
+        username = action[1]
+        if username not in system:
+            print(f"ERROR: user {username} not found")
+        else:
+            print(f"{username} unregistered successfully")
+            system.pop(username)
+    elif "register" == action[0]:
+        action_type = action[0]
+        username = action[1]
+        license_plate_number = action[2]
+        if username not in system:
+            system[username] = license_plate_number
+            print(f"{username} registered {license_plate_number} successfully")
+        else:
+            print(f"ERROR: already registered with plate number {license_plate_number}")
+
+for user, plate in system.items():
+    print(f"{user} => {plate}")
