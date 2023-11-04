5aca82eea2db530ee8be83d8e9fb3ca64260e74a
Viviancat666 <vivian9069@gmail.com>
2023-11-04 20:45:17 +0200

    Update moba_challenger.py

--- Fundamentals/dictionaries_more_exercises/moba_challenger.py
+++ Fundamentals/dictionaries_more_exercises/moba_challenger.py
@@ -14,13 +14,25 @@ while command != "Season end":
             players[player][position] = skill
 
     else:
-        player_one, player_two = command.split("vs")
+        player_one, player_two = command.split(" vs ")
+        if player_one in players and player_two in players:
+            demoted_player = None
+            for pos in players[player_one].keys():
+                for p in players[player_two].keys():
+                    if pos == p:
+                        if sum(players[player_one].values()) > sum(players[player_two].values()):
+                            demoted_player = player_two
+                        elif sum(players[player_one].values()) < sum(players[player_two].values()):
+                            demoted_player = player_one
+            if demoted_player:
+                players.pop(demoted_player)
+
     command = input()
 
-for player, info in players.items():
-    total_points = 0
-    for position, skill in info.items():
-        total_points += skill
-    print(f"{player}: {total_points}")
-    for position, skill in info.items():
+sorted_skills = dict(sorted(players.items(), key=lambda x: (-sum(x[1].values()), x[0])))
+
+for player, info in sorted_skills.items():
+    print(f"{player}: {sum(info.values())} skill")
+    sorted_names = dict(sorted(info.items(), key=lambda x: (-x[1], x[0])))
+    for position, skill in sorted_names.items():
         print(f"- {position} <::> {skill}")
