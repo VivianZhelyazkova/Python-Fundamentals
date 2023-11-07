e1f6015a104a331338722092bb9fc9a5b1027ee2
Viviancat666 <vivian9069@gmail.com>
2023-11-07 02:48:09 +0200

    Create reverse_strings.py

---
+++ Fundamentals/text_processing/reverse_strings.py
@@ -0,0 +1,15 @@
+string = input()
+
+while string != "end":
+    # reversed_list = [char for char in reversed(string)]
+    # reversed_string = "".join(reversed_list)
+    # print(f"{string} = {reversed_string}")
+
+    reversed_list = list(string)
+    reversed_list.reverse()
+    reversed_string = "".join(reversed_list)
+    print(f"{string} = {reversed_string}")
+
+    string = input()
+
+
