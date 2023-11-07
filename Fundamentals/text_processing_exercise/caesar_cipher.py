19dde2de3715065340b3729240dcc69418e64b05
Viviancat666 <vivian9069@gmail.com>
2023-11-07 21:55:16 +0200

    Update caesar_cipher.py

--- Fundamentals/text_processing_exercise/caesar_cipher.py
+++ Fundamentals/text_processing_exercise/caesar_cipher.py
@@ -1,9 +1,10 @@
 text = input()
 
-encrypted = ""
+# encrypted = ""
+#
+# for char in text:
+#
+#     encrypted += chr(ord(char) + 3)
 
-for char in text:
-
-    encrypted += chr(ord(char) + 3)
-
+encrypted = "".join(chr(ord(char) + 3) for char in text)
 print(encrypted)
