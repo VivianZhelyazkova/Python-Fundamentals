84828afcd082216a3e8d1188f55fbb15318a21eb
Viviancat666 <vivian9069@gmail.com>
2023-11-07 02:58:59 +0200

    Update repeat_strings.py

--- Fundamentals/text_processing/repeat_strings.py
+++ Fundamentals/text_processing/repeat_strings.py
@@ -1,8 +1,10 @@
 sequence = input().split()
-repeated_sequence = ""
+# repeated_sequence = ""
+#
+# for string in sequence:
+#
+#     repeated_sequence += string * len(string)
 
-for string in sequence:
-
-    repeated_sequence += string * len(string)
+repeated_sequence = "".join([string * len(string) for string in sequence])
 
 print(repeated_sequence)
