d30725e29bfdf6eedfb4e2c65bcac26110b426fc
Viviancat666 <vivian9069@gmail.com>
2023-11-07 02:49:32 +0200

    Update reverse_strings.py

--- Fundamentals/text_processing/reverse_strings.py
+++ Fundamentals/text_processing/reverse_strings.py
@@ -5,11 +5,13 @@ while string != "end":
     # reversed_string = "".join(reversed_list)
     # print(f"{string} = {reversed_string}")
 
-    reversed_list = list(string)
-    reversed_list.reverse()
-    reversed_string = "".join(reversed_list)
-    print(f"{string} = {reversed_string}")
+    # reversed_list = list(string)
+    # reversed_list.reverse()
+    # reversed_string = "".join(reversed_list)
+    # print(f"{string} = {reversed_string}")
 
+    reversed_string = "".join(reversed(string))
+    print(f"{string} = {reversed_string}")
     string = input()
 
 
