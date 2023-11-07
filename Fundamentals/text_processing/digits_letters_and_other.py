6013978bb12c313cbbdeb4254e552c859fed175e
Viviancat666 <vivian9069@gmail.com>
2023-11-07 03:35:01 +0200

    Update digits_letters_and_other.py

--- Fundamentals/text_processing/digits_letters_and_other.py
+++ Fundamentals/text_processing/digits_letters_and_other.py
@@ -1,16 +1,16 @@
 string = input()
 
-digits = ""
-letters = ""
-characters = ""
+digits = "".join([char for char in string if char.isdigit()])
+letters = "".join([char for char in string if char.isalpha()])
+characters = "".join([char for char in string if not char.isalnum()])
 
-for char in string:
-    if char.isdigit():
-        digits += char
-    elif char.isalpha():
-        letters += char
-    else:
-        characters += char
+# for char in string:
+#     if char.isdigit():
+#         digits += char
+#     elif char.isalpha():
+#         letters += char
+#     else:
+#         characters += char
 
 print(digits)
 print(letters)
