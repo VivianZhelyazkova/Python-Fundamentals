047b47c60a15524a3995007f55cc8b4ad3533652
Viviancat666 <vivian9069@gmail.com>
2023-11-09 02:32:51 +0200

    Function added

--- Fundamentals/text_processing_exercise/letters_change_numbers.py
+++ Fundamentals/text_processing_exercise/letters_change_numbers.py
@@ -1,29 +1,43 @@
+from enum import Enum
+
 strings = input().split()
 results = []
+
+
+class Operation(Enum):
+    DIVIDE = 1
+    MULTIPLY = 2
+    SUBTRACT = 3
+    ADD = 4
+
+
+def result(letter, num, max_res, operation: Operation):
+    letter_value = ord(letter.lower()) - 96
+    current_result = 0
+    match operation:
+        case Operation.DIVIDE:
+            current_result = num / letter_value
+        case Operation.MULTIPLY:
+            current_result = num * letter_value
+        case Operation.SUBTRACT:
+            current_result = max_res - letter_value
+        case Operation.ADD:
+            current_result = max_res + letter_value
+    return current_result
+
+
 for string in strings:
     max_result = 0
     number = int("".join(list(n for n in string if n.isdigit())))
     first_letter, second_letter = string[0], string[-1]
     if first_letter.isupper():
-        letter_lower = first_letter.lower()
-        letter_value = ord(letter_lower) - 96
-        current_result = number / letter_value
-        max_result = max(current_result, max_result)
+        max_result = result(first_letter, number, max_result, Operation.DIVIDE)
     elif first_letter.islower():
-        letter_lower = first_letter.lower()
-        letter_value = ord(letter_lower) - 96
-        current_result = number * letter_value
-        max_result = max(current_result, max_result)
+        max_result = result(first_letter, number, max_result, Operation.MULTIPLY)
     if second_letter.isupper():
-        letter_lower = second_letter.lower()
-        letter_value = ord(letter_lower) - 96
-        current_result = max_result - letter_value
-        max_result = current_result
+        max_result = result(second_letter, number, max_result, Operation.SUBTRACT)
     elif second_letter.islower():
-        letter_lower = second_letter.lower()
-        letter_value = ord(letter_lower) - 96
-        current_result = max_result + letter_value
-        max_result = current_result
+        max_result = result(second_letter, number, max_result, Operation.ADD)
     results.append(max_result)
 
 print(f"{sum(results):.2f}")
