4cbdc55706fae929bc1439f8abbec50fd29d5ee7
Viviancat666 <vivian9069@gmail.com>
2023-11-10 04:03:28 +0200

    Matrix moved

--- Fundamentals/matrix/01.py
+++
@@ -1,9 +0,0 @@
-numbers = [int(x) for x in input().split()]
-
-
-def is_positive(some_list):
-    positive_numbers = list(filter(lambda s: s > 0, some_list))
-    return len(positive_numbers) == len(some_list)
-
-
-print(is_positive(numbers))
