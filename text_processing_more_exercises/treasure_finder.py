b2ed11a293927be02131d53bcfb0810a82dfd229
Viviancat666 <vivian9069@gmail.com>
2023-11-12 21:44:12 +0200

    Create treasure_finder.py

---
+++ text_processing_more_exercises/treasure_finder.py
@@ -0,0 +1,29 @@
+keys = [int(x) for x in input().split()]
+command = input()
+
+treasure = ""
+coordinates = ""
+
+while command != "find":
+    key_index = 0
+    decrypted_message = ""
+    for char in command:
+        new_char = chr(ord(char) - keys[key_index])
+        decrypted_message += new_char
+        key_index = (key_index + 1) % len(keys)
+    treasure = ""
+    coordinates = ""
+    treasure_found = False
+    for char in decrypted_message:
+        if treasure_found and char != "&":
+            treasure += char
+        if char == "&":
+            treasure_found = not treasure_found
+    coordinates_found = False
+    for char in decrypted_message:
+        if coordinates_found and char != ">":
+            coordinates += char
+        if char == "<":
+            coordinates_found = True
+    print(f"Found {treasure} at {coordinates}")
+    command = input()
