8924d4ce85e92bfec8cf24d613a237cd8690bff3
Viviancat666 <vivian9069@gmail.com>
2023-11-14 02:49:27 +0200

    Update find_occurrences_of_word_in_sentence.py

--- regular_expressions_exercise/find_occurrences_of_word_in_sentence.py
+++ regular_expressions_exercise/find_occurrences_of_word_in_sentence.py
@@ -1,12 +1,10 @@
 import re
 
 string = input().lower()
-word_to_find = input()
+word_to_find = input().lower()
 regex = f"\\b{word_to_find}\\b"
 # pattern = re.compile(r'\b' + re.escape(word_to_find) + r'\b', re.IGNORECASE)
 
 matches = re.findall(regex, string)
-print(string)
-print(matches)
 count = len(matches)
 print(count)
