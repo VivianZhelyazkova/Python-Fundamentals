8d396e301afdc5e4de6902ec817b41d20c92edfa
Viviancat666 <vivian9069@gmail.com>
2023-12-01 02:27:24 +0200

    Update emoji_detector.py

--- exam_preparation/emoji_detector.py
+++ exam_preparation/emoji_detector.py
@@ -7,7 +7,7 @@ numbers = list(int(x) for x in text if x.isnumeric())
 threshold = math.prod(numbers)
 print(threshold)
 
-regex = r"([:*]{2})([A-Z][a-z]{2,})\1"
+regex = r"([:*]{2})([A-Z][a-z]{2,})(\1)"
 matches = re.finditer(regex, text)
 
 emojies = []
@@ -15,4 +15,12 @@ cool_emojies = []
 
 for match in matches:
     emojies.append(match.group(2))
+    ascii_sum = sum(list(ord(x) for x in match.group(2)))
+    if ascii_sum >= threshold:
+        cool_emojies.append("".join(match.groups()))
 
+print(f"Cool threshold: {threshold}")
+print(f"{len(emojies)} emojis found in the text")
+print(f"The cool ones are: ")
+for emoji in cool_emojies:
+    print(emoji)
